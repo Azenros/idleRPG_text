ba7ceeff79797faec495dd205803191ab126ef5a
repl.it user <replituser@example.com>
2020-08-01 21:52:21 +0000

    Add levels and experience

--- characters.h
+++ characters.h
@@ -4,6 +4,10 @@
 #include "includes.h"
 #include "item.h"
 
+std::vector<int> toNextLvl = {0, 1, 2, 4, 8, 16, 
+                         32, 64, 128, 256, 
+                         1024, 2048};
+
 class Entity {
   private:
     int health = 10; //hp
@@ -11,40 +15,57 @@ class Entity {
     std::string name = "Nameless";
     int attack = 0; //atk
     int defense = 0; //def
+    int level = 1; 
+    int expHeld = 0;
 
   protected:
     void setName(std::string s) { name = s; };
-    void setHealth(int);
-    void setAtk(int);
-    void setDef(int);
-
+    void setHealth(int h) { health = h; };
+    void setMaxHP (int h) { maxhealth = h; };
+    void setAtk(int a) { attack = a; };
+    void setDef(int d) { defense = d; };
+    void setLevel(int l) { level = l; };
+    void setExp(int e) { expHeld = e; };
 
   public:
-    Entity(int i, std::string s, int a, int d) 
-      : health(i), name(s), attack(a), defense(d) {};
+    Entity(int h, std::string s, int a, int d) 
+      : health(h), maxhealth(h), name(s), attack(a), defense(d) {};
 
     int getHP() { return health; };
+    int getMaxHP() { return maxhealth; };
     int getAtk() { return attack; };
-    int getDef() {return defense; };
+    int getDef() { return defense; };
+    int getLvl() { return level; };
+    int getExp() { return expHeld; };
     std::string getName() { return name; };
     
-
 };
 
 class Player : public Entity {
   private:
     int money = 0;
+    int exptoNext = 1;
+
   public:
     Player(int i, std::string s, int a, int d) 
       : Entity(i,s,a,d) {};
 
     int getMoney() { return money; };
+    int getNext() { return exptoNext; };
+    void setMoney(int m) { money += m; };
+    void addExp(int e) { setExp(getExp() + e); };
+    void levelUp() {
+      int l = getLvl();
+      setLevel(l++);
+      exptoNext = toNextLvl[l];
+    } 
     
 };
 
 class Monster : public Entity {
   protected:
     int moneyOnKill = 10;
+
   public:
     Monster(int i, std::string s, int a, int d) 
       : Entity(i,s,a,d) {}
@@ -52,6 +73,13 @@ class Monster : public Entity {
       : Entity(i,s,a,d), moneyOnKill(m) {};
 
     int getMoney() { return moneyOnKill; };
+    void onDefeat(Player p) {
+      p.setMoney(moneyOnKill);
+      p.addExp(getExp());
+      if (p.getExp() >= p.getNext()) {
+        p.levelUp();
+      }
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "includes.h"
 #include "characters.h"
 
+void printStats(Entity e) {
+  std::cout << std::endl
+            << e.getName() << ":\n__________________\n\n"
+            << "Level " << e.getLvl() << std::endl
+            << "EXP: " << e.getExp() << std::endl
+            << "HP: " << e.getHP() << " / " << e.getMaxHP() << std::endl
+            << "ATK: " << e.getAtk() << std::endl
+            << "DEF: " << e.getDef() << std::endl
+            << std::endl;
+}
 
 int main() {
   Player p(10, "bob", 1, 0);
@@ -8,9 +18,13 @@ int main() {
 
 
   std::cout << "Test:\n";
-  std::cout << "player: " << p.getName() << ": "<< p.getHP() << " HP, " << p.getAtk() <<" Atk, " << p.getDef() << " Def, ";
+  std::cout << "player: " << p.getName() << ": "<< p.getHP() << " HP, " << p.getAtk() <<" Atk, " << p.getDef() << " Def\n";
   std::cout << std::endl;
-  std::cout << "mob: " << m.getName() << ": "<< m.getHP() << " HP, " << m.getAtk() <<" Atk, " << m.getDef() << " Def, ";
+  std::cout << "mob: " << m.getName() << ": "<< m.getHP() << " HP, " << m.getAtk() <<" Atk, " << m.getDef() << " Def\n";
+
+  std::cout << "Test2:\n";
+  printStats(p);
+  printStats(m);
 
   
 }
