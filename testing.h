fc5916520e0c80dbaba130dd3be4dc9c08849297
repl.it user <replituser@example.com>
2020-08-17 19:56:00 +0000

    Add new method of printing stats

--- testing.h
+++ testing.h
@@ -1,15 +1,23 @@
 #ifndef TESTING_H
 #define TESTING_H
 
+#include "includes.h"
 #include "characters.h"
 
-std::string line = "__________________\n"
+std::string line = "__________________";
+std::string space = "                  ";
+
+void printChar(int i, char c) {
+  for (int k = 0; k < i; k++) {
+    std::cout << c;
+  }
+}
 
 struct Testing {
 
   void printEntStats (Entity e) { //print basic parameters
     std::cout << std::endl
-            << e.getName() << ":\n" << line << "\n"
+            << e.getName() << ":\n" << line << "\n\n"
             << "Level " << e.getLvl() << std::endl
             << "EXP: " << e.getExp() << std::endl
             << "HP: " << e.getHP() << " / " << e.getMaxHP() << std::endl
@@ -25,7 +33,7 @@ struct Testing {
     printEntStats(p);
     std::cout << "To Next Level: " << p.getNext() << std::endl
               << "Gold: " << p.getGold() << std::endl
-              << std::endl << line;
+              << std::endl << line << std::endl;
 
   };
 
@@ -37,6 +45,70 @@ struct Testing {
               << std::endl << line << std::endl;
 
   };
+
+  void twinStats(Player p, Monster m) {
+    //Prints stats of player and mob on same lines
+    //Adapts to length of names
+    int position = (p.getName().size() + 1) + 7;
+    int spaces = 0;
+
+    //names
+    spaces = position - (p.getName().size() + 1);
+
+    std::cout << std::endl << p.getName() << ":";
+    printChar(spaces, ' ');
+    std::cout << m.getName() << ":\n";
+
+    //line
+    printChar(position + m.getName().size() + 2, '_');
+    std::cout << std::endl << std::endl;
+    
+    //level
+    std::string lvlstr = std::to_string(p.getLvl());
+    spaces = position - (6 + lvlstr.size());
+
+    std::cout << "Level " << lvlstr;
+    printChar(spaces, ' ');
+    std::cout << "Level " << m.getLvl() << std::endl;
+
+    //EXP
+    std::string curExp = std::to_string(p.getExp());
+    std::string nextExp = std::to_string(p.getNext());
+    spaces = position - (8 + curExp.size() + nextExp.size());
+
+    std::cout << "EXP: " << p.getExp() << " / " << p.getNext();
+    printChar(spaces, ' '); 
+    std::cout << "EXP: " << m.getExp() << std::endl;
+
+    //HP
+    std::string curHP = std::to_string(p.getHP());
+    std::string maxHP = std::to_string(p.getMaxHP());
+    spaces = position - (7 + curHP.size() + maxHP.size());
+
+    std::cout << "HP: " << p.getHP() << " / " << p.getMaxHP();
+    printChar(spaces, ' ');
+    std::cout << "HP: " << m.getHP() << " / " << m.getMaxHP() << std::endl;
+
+    //ATK
+    std::string atk = std::to_string(p.getAtk());
+    spaces = position - (5 + atk.size());
+
+    std::cout << "ATK: " << p.getAtk();
+    printChar(spaces, ' ');
+    std::cout << "ATK: " << m.getAtk() << std::endl;
+
+    //DEF
+    std::string def = std::to_string(p.getDef());
+    spaces = position - (5 + atk.size());
+
+    std::cout << "DEF: " << p.getDef();
+    printChar(spaces, ' ');
+    std::cout << "DEF: " << m.getDef() << std::endl;
+
+    
+
+
+  }
 
 };
 
